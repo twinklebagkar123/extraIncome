d9cc10ab0a0943a0d3a587f9d77b2c461e13299f
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-24 01:20:48 +0530

    home 2

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -1,5 +1,13 @@
 <?php
 /* template name: Homepage Template */
 get_header();
+?>
+<section class="home-banner">
+    <div class="container">
+        <div class="col-8">hi</div>
+        <div class="col-4">col-4</div>
+    </div>
+</section>
+<?php
 get_footer();
 ?>
