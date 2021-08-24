7196f6a960bc13987ccf39ea8e675d41dcecd3ef
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-24 14:05:53 +0530

    get posts home

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -5,8 +5,48 @@ get_header();
 <section class="home-banner">
     <div class="container">
         <div class="row">
-            <div class="col-8">hi</div>
-            <div class="col-4">col-4</div>
+            <div class="col-8">
+            <?php 
+                // the query
+                $the_query = new WP_Query( array(
+                    'posts_per_page' => 1,
+                )); 
+                ?>
+
+                <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+                    <?php the_title(); ?>
+                    <?php the_excerpt(); ?>
+
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+
+                <?php else : ?>
+                <p><?php __('No News'); ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="col-4"><?php
+                <?php 
+                // the query
+                $the_query = new WP_Query( array(
+                    'posts_per_page' => 2,
+                )); 
+                ?>
+
+                <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+                    <?php the_title(); ?>
+                    <?php the_excerpt(); ?>
+
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+
+                <?php else : ?>
+                <p><?php __('No News'); ?></p>
+                <?php endif; ?>
+            ?></div>
         </div>
         
     </div>
