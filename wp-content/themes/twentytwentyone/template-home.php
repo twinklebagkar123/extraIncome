f53459cc89e9962625985590cc111e1c58740e98
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-24 17:58:01 +0530

    section latest posts

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -91,6 +91,43 @@ get_header();
                         <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
                         <h1 class="innertitle">Latest Posts</h1>
                     </div>
+                    <div class="section-blogs">
+                    <?php
+                // the query
+                $the_query = new WP_Query( array(
+                    'posts_per_page' => 6,
+                )); 
+                ?>
+
+                <?php if ( $the_query->have_posts() ) : ?>
+                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <div class="wrapper">
+                    <div class="blog-inner position-relative">
+                        <a href="<?php the_permalink();?>">
+                            <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
+                        </a>
+                        <div class="blog-content position-absolute s-content">
+			                <div class="post-meta">
+                                <div class="blog-cat">
+						            <a href="" class="category-tag">Life Style new</a>					
+                                </div>
+								<div class="blog-date">
+						            <a href="https://deeptem.com/blog/2021/03/01/start-new-season-with-latest-zaras-collection/">1 March 2021</a>
+					            </div>
+								<div class="clearfix"></div>
+			                </div>
+                            <h3 class="post-title"><a href="https://deeptem.com/blog/2021/03/01/start-new-season-with-latest-zaras-collection/"><?php the_title();?></a></h3>
+                            
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+
+                <?php else : ?>
+                <p><?php __('No News'); ?></p>
+                <?php endif; ?>
+                    </div>
                 </div>
 
             </div>
