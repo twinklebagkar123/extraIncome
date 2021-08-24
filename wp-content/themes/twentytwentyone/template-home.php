ee1eb31662893cb97306fa7a83df80ea0fa2b721
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-24 12:01:34 +0530

    homepage

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -4,8 +4,11 @@ get_header();
 ?>
 <section class="home-banner">
     <div class="container">
-        <div class="col-8">hi</div>
-        <div class="col-4">col-4</div>
+        <div class="row">
+            <div class="col-8">hi</div>
+            <div class="col-4">col-4</div>
+        </div>
+        
     </div>
 </section>
 <?php
