8b27118b000da6c3df53721fcea4d7eafba4935e
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-24 16:05:43 +0530

    side post

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -51,8 +51,24 @@ get_header();
                 <?php if ( $the_query->have_posts() ) : ?>
                 <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-                    <?php the_title(); ?>
-                    <?php the_excerpt(); ?>
+                <div class="blog-inner position-relative">
+                        <a href="<?php the_permalink();?>">
+                            <img class="img-fluid blog-img" src="<? the_post_thumbnail_url(); ?>">
+                        </a>
+                        <div class="blog-content position-absolute">
+			                <div class="post-meta">
+                                <div class="blog-cat">
+						            <a href="" class="category-tag">Life Style new</a>					
+                                </div>
+								<div class="blog-date">
+						            <a href="https://deeptem.com/blog/2021/03/01/start-new-season-with-latest-zaras-collection/">1 March 2021</a>
+					            </div>
+								<div class="clearfix"></div>
+			                </div>
+                            <h3 class="post-title"><a href="https://deeptem.com/blog/2021/03/01/start-new-season-with-latest-zaras-collection/"><?php the_title();?></a></h3>
+                            
+                        </div>
+                    </div>
 
                 <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
