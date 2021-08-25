8e9edc86bf18c36b9b98937773f9786dffcaeec4
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-25 22:42:51 +0530

    footer

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -16,54 +16,56 @@
 		</div><!-- #primary -->
 	</div><!-- #content -->
 
+<section class="footer">
+<footer id="colophon" class="site-footer" role="contentinfo">
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-		<div class="site-info">
-			<div class="site-name">
-				<?php if ( has_custom_logo() ) : ?>
-					<div class="site-logo"><?php the_custom_logo(); ?></div>
+<?php if ( has_nav_menu( 'footer' ) ) : ?>
+	<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
+		<ul class="footer-navigation-wrapper">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'footer',
+					'items_wrap'     => '%3$s',
+					'container'      => false,
+					'depth'          => 1,
+					'link_before'    => '<span>',
+					'link_after'     => '</span>',
+					'fallback_cb'    => false,
+				)
+			);
+			?>
+		</ul><!-- .footer-navigation-wrapper -->
+	</nav><!-- .footer-navigation -->
+<?php endif; ?>
+<div class="site-info">
+	<div class="site-name">
+		<?php if ( has_custom_logo() ) : ?>
+			<div class="site-logo"><?php the_custom_logo(); ?></div>
+		<?php else : ?>
+			<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
+				<?php if ( is_front_page() && ! is_paged() ) : ?>
+					<?php bloginfo( 'name' ); ?>
 				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						<?php endif; ?>
-					<?php endif; ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 				<?php endif; ?>
-			</div><!-- .site-name -->
-			<div class="powered-by">
-				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
-				);
-				?>
-			</div><!-- .powered-by -->
+			<?php endif; ?>
+		<?php endif; ?>
+	</div><!-- .site-name -->
+	<div class="powered-by">
+		<?php
+		printf(
+			/* translators: %s: WordPress. */
+			esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
+			'<a href="' . esc_url( __( 'https://wordpress.org/', 'twentytwentyone' ) ) . '">WordPress</a>'
+		);
+		?>
+	</div><!-- .powered-by -->
 
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</div><!-- .site-info -->
+</footer><!-- #colophon -->
+</section>
+
 
 </div><!-- #page -->
 
