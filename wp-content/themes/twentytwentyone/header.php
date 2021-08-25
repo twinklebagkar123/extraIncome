aa08df2d1236622793ddc8210c631a0f9d36c55c
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-25 22:16:32 +0530

    header Update

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -24,8 +24,10 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
-
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+	<div class="header-bg">
+		<?php get_template_part( 'template-parts/header/site-header' ); ?>
+	</div>
+	
 
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
