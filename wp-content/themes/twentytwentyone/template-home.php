a7753be73bd472d789a64b499e4acaae3da0bbbc
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-26 15:48:08 +0530

    aboutside

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -86,91 +86,46 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 "><?php
-                // the query
-                $the_query = new WP_Query( array(
-                    'posts_per_page' => 2,
-                )); 
-                ?>
-
-                <?php if ( $the_query->have_posts() ) : ?>
-                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                <div class="wrapper">
-                    <div class="blog-inner position-relative">
-                        <a href="<?php the_permalink();?>">
-                            <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
-                        </a>
-                        <div class="blog-content position-absolute s-content">
-			                <div class="post-meta">
-                                <div class="blog-cat">
-						            <a href="" class="category-tag">Life Style new</a>					
-                                </div>
-								<div class="blog-date">
-						            <a href="">1 March 2021</a>
-					            </div>
-								<div class="clearfix"></div>
-			                </div>
-                            <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-                            
-                        </div>
-                    </div>
-                </div>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-
-                <?php else : ?>
-                <p><?php __('No News'); ?></p>
-                <?php endif; ?>
-            </div>
-        </div>
-        
-    </div>
-</section>
-<section class="section2">
-        <div class="container">
-            <div class="row">
-                <div class="col-8">
-                    <div class="section-title">
-                        <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
-                        <h1 class="innertitle">Latest Posts</h1>
-                    </div>
-                    <div class="section-blogs">
+            <div class="col-sm-4 ">
+                <div class="section1">
                     <?php
-                // the query
-                $the_query = new WP_Query( array(
-                    'posts_per_page' => 6,
-                )); 
-                ?>
+                    // the query
+                    $the_query = new WP_Query( array(
+                        'posts_per_page' => 2,
+                    )); 
+                    ?>
 
-                <?php if ( $the_query->have_posts() ) : ?>
-                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                <div class="blog-wrapper">
-                    <div class="omega">
-                        <div class="blogImage">
-                            <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
-                        </div>
-                        <div class="blogContent">
-                            <div class="postmetadata">
-                                <h6 class="blog-date">
-                                    <i class="pe-7s-clock"></i><?php the_date();?>
-                                </h6>
+                    <?php if ( $the_query->have_posts() ) : ?>
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                    <div class="wrapper">
+                        <div class="blog-inner position-relative">
+                            <a href="<?php the_permalink();?>">
+                                <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
+                            </a>
+                            <div class="blog-content position-absolute s-content">
+                                <div class="post-meta">
+                                    <div class="blog-cat">
+                                        <a href="" class="category-tag">Life Style new</a>					
+                                    </div>
+                                    <div class="blog-date">
+                                        <a href="">1 March 2021</a>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+                                
                             </div>
-                            <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3> 
-                            <p>When a blog crosses over into personal stories and emotions, it’s now entered into the realm of...</p>  
                         </div>
-
                     </div>
-                </div>
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
 
-                <?php else : ?>
-                <p><?php __('No News'); ?></p>
-                <?php endif; ?>
-                    </div>
+                    <?php else : ?>
+                    <p><?php __('No News'); ?></p>
+                    <?php endif; ?>
                 </div>
-                <div class="col-4">
-                    <div class="section-title">
+                <div class="aboutside">
+                <div class="section-title">
                         <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
                         <h1 class="innertitle">About us</h1>
                     </div>
@@ -193,6 +148,18 @@ get_header();
                     <div class="mt-4 sidebar">
                     <?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
                     </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
+<section class="section2">
+        <div class="container">
+            <div class="row">
+              
+                <div class="col-4">
+                    
                 </div>
 
             </div>
