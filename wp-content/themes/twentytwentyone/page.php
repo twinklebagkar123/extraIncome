e7318f0895071f0725e23c9c5044448d08d606c2
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-26 18:35:27 +0530

    page

--- wp-content/themes/twentytwentyone/page.php
+++ wp-content/themes/twentytwentyone/page.php
@@ -11,18 +11,6 @@
 
 get_header();
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-	?>
-	<h1>Hi</h1>
-	<?php
 
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
 
 get_footer();
