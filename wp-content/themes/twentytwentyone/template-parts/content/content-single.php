eb6897ff6c972206a286bdb86b52fa4cd1d00f65
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-26 23:57:10 +0530

    single

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -12,32 +12,27 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<div class="breadcrumbs">
+<div class="title">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
+	</div>
+	<div class="thumbnail">
+		<?php twenty_twenty_one_post_thumbnail(); ?>
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</div>
+	<div class="content">
+		<?php the_content(); ?>
+		
+</div>
 
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-8">
-				<div class="title">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-					
-				</div>
-				<div class="thumbnail">
-					<?php twenty_twenty_one_post_thumbnail(); ?>
-					<?php twenty_twenty_one_entry_meta_footer(); ?>
-				</div>
-				<div class="content">
-					<?php the_content(); ?>
-					
-				</div>
-			</div>
-			<div class="col-sm-4 navigation">
-			
-			<?php
+
+	<header class="entry-header alignwide">
+		
+		
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+	<?php
 						wp_link_pages(
 							array(
 								'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
@@ -47,23 +42,12 @@
 							)
 						);
 					?>
-			</div>
-		</div>
-	</div>
-
-	<header class="entry-header alignwide">
-		
-		
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		
 	</div><!-- .entry-content -->
 
 	
 
-	<?php //if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php //get_template_part( 'template-parts/post/author-bio' ); ?>
-	<?php //endif; ?>
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
+	<?php endif; ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
