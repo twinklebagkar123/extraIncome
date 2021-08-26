d47b30a351d5d3491a21801f486c220db9dde77d
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-26 16:05:51 +0530

    css

--- wp-content/themes/twentytwentyone/template-home.php
+++ wp-content/themes/twentytwentyone/template-home.php
@@ -46,44 +46,7 @@ get_header();
                 <?php endif; ?>
             </div>
                 <div class="latestPost">
-                    <div class="section-title">
-                        <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
-                        <h1 class="innertitle">Latest Posts</h1>
-                    </div>
-                    <div class="section-blogs">
-                            <?php
-                        // the query
-                        $the_query = new WP_Query( array(
-                            'posts_per_page' => 6,
-                        )); 
-                        ?>
-
-                        <?php if ( $the_query->have_posts() ) : ?>
-                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                        <div class="blog-wrapper">
-                            <div class="omega">
-                                <div class="blogImage">
-                                    <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
-                                </div>
-                                <div class="blogContent">
-                                    <div class="postmetadata">
-                                        <h6 class="blog-date">
-                                            <i class="pe-7s-clock"></i><?php the_date();?>
-                                        </h6>
-                                    </div>
-                                    <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3> 
-                                    <p>When a blog crosses over into personal stories and emotions, it’s now entered into the realm of...</p>  
-                                </div>
-
-                            </div>
-                        </div>
-                        <?php endwhile; ?>
-                        <?php wp_reset_postdata(); ?>
-
-                        <?php else : ?>
-                        <p><?php __('No News'); ?></p>
-                        <?php endif; ?>
-                    </div>
+                    
                 </div>
             </div>
             <div class="col-sm-4 ">
@@ -124,6 +87,56 @@ get_header();
                     <p><?php __('No News'); ?></p>
                     <?php endif; ?>
                 </div>
+                
+            </div>
+        </div>
+        
+    </div>
+</section>
+<section class="section2">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                <div class="section-title">
+                        <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
+                        <h1 class="innertitle">Latest Posts</h1>
+                    </div>
+                    <div class="section-blogs">
+                            <?php
+                        // the query
+                        $the_query = new WP_Query( array(
+                            'posts_per_page' => 6,
+                        )); 
+                        ?>
+
+                        <?php if ( $the_query->have_posts() ) : ?>
+                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <div class="blog-wrapper">
+                            <div class="omega">
+                                <div class="blogImage">
+                                    <img class="img-fluid blog-img s-blog-img" src="<? the_post_thumbnail_url(); ?>">
+                                </div>
+                                <div class="blogContent">
+                                    <div class="postmetadata">
+                                        <h6 class="blog-date">
+                                            <i class="pe-7s-clock"></i><?php the_date();?>
+                                        </h6>
+                                    </div>
+                                    <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3> 
+                                    <p>When a blog crosses over into personal stories and emotions, it’s now entered into the realm of...</p>  
+                                </div>
+
+                            </div>
+                        </div>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
+
+                        <?php else : ?>
+                        <p><?php __('No News'); ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+              <div class="col-sm-4">
                 <div class="aboutside">
                 <div class="section-title">
                         <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
@@ -149,17 +162,6 @@ get_header();
                     <?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
                     </div>
                 </div>
-            </div>
-        </div>
-        
-    </div>
-</section>
-<section class="section2">
-        <div class="container">
-            <div class="row">
-              
-                <div class="col-4">
-                    
                 </div>
 
             </div>
