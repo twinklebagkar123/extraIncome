8fcbd68026929bd6714d491f6130df76357da6f3
twinklebagkar123 <51827860+twinklebagkar123@users.noreply.github.com>
2021-08-26 23:26:52 +0530

    singles

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -12,30 +12,55 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="breadcrumbs">
+
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8">
+				<div class="title">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					
+				</div>
+				<div class="thumbnail">
+					<?php twenty_twenty_one_post_thumbnail(); ?>
+				</div>
+				<div class="content">
+					<?php the_content(); ?>
+					<?php
+						wp_link_pages(
+							array(
+								'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+								'after'    => '</nav>',
+								/* translators: %: Page number. */
+								'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+							)
+						);
+					?>
+				</div>
+			</div>
+			<div class="col-sm-4">
+			<footer class="entry-footer default-max-width">
+				<?php twenty_twenty_one_entry_meta_footer(); ?>
+			</footer><!-- .entry-footer -->
+			</div>
+		</div>
+	</div>
 
 	<header class="entry-header alignwide">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twenty_twenty_one_post_thumbnail(); ?>
+		
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
+		
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
+	
 
 	<?php if ( ! is_singular( 'attachment' ) ) : ?>
 		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
