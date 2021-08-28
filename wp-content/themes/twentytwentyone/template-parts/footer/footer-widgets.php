8a0df07e60457f0b3307e85d3b240f99702b965b
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-08-28 13:40:01 +0530

    sidebar

--- wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
@@ -6,6 +6,19 @@
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
+?>
+<div class="section-title">
+                        <span class="wn-deep-title-shape elementor-repeater-item-848bb03 after"></span>
+                        <h1 class="innertitle">Categories</h1>
+</div>
+<h2>Collapsibles</h2>
+
+<p>A Collapsible:</p>
+<button type="button" class="collapsible">Open Collapsible</button>
+<div class="content">
+  <?php the_content(); ?>
+</div>
+<?php
 
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
