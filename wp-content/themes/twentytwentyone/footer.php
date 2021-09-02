c98bf7b5b3dbfffa02d09eaf2be913ce4d17060a
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-09-02 12:55:02 +0530

    scroll to top

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -78,7 +78,10 @@
 
 </footer><!-- #colophon -->
 </section>
-
+<!-- Back to top button -->
+<button type="button" class="btn btn-danger btn-floating btn-lg" id="btn-back-to-top">
+  <i class="fas fa-arrow-up"></i>
+</button>
 
 </div><!-- #page -->
 
@@ -97,6 +100,31 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 </script>
 </body>
